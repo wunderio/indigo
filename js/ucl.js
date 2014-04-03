2f59b6f24933a5aa2a26f76b4b1186bd93fa6404
sturobson <stu@alwaystwisted.com>
2014-04-03 11:00:35 +0100

    adds debounce to fix resize errors  by restoring defaults

--- js/ucl.js
+++ js/ucl.js
@@ -944,3 +944,13 @@ $('.close-me').click(function() {
 });
 
 
+function debounce(a,b,c){var d;return function(){var e=this,f=arguments;clearTimeout(d),d=setTimeout(function(){d=null,c||a.apply(e,f)},b),c&&!d&&a.apply(e,f)}}
+var myEfficientFn = debounce(function() {
+  // All the taxing stuff you do
+    $('.facets').removeClass('show-facet');
+  $('.search-meta, .search-results').removeClass('move');
+  $('.close-me').removeClass('showit');
+  $('.off-canvas').removeClass('spin');
+}, 50);
+
+window.addEventListener('resize', myEfficientFn);
