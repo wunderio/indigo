99b1fa8e1d493ce17f2c4b956968bfa74473584d
sturobson <stu@alwaystwisted.com>
2014-06-17 18:08:14 +0100

    updates CSS/HTML/JS for all UX refinements

--- js/ucl.js
+++ js/ucl.js
@@ -157,3 +157,35 @@ $(window).resize(function() {
   else {
   }
 });
+
+
+(function( $ ){
+
+
+  $.fn.appendAround = function(){
+    return this.each(function(){
+
+      var $self = $( this ),
+          att = "data-set",
+          $set = $( "["+ att +"='"+ $self.closest( "["+ att +"]" ).attr( att ) + "']" );
+
+      function appendToVisibleContainer(){
+        if( $self.is( ":hidden" ) ){
+          $self.appendTo( $set.filter( ":visible:eq(0)" ) );
+        }
+      }
+
+      appendToVisibleContainer();
+
+      $(window).resize( appendToVisibleContainer );
+
+    });
+  };
+
+
+
+  //try it out
+  $( ".pillbox--inner" ).appendAround();
+
+
+}( jQuery ));
